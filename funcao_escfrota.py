c0585f74e4bbc109482044ac4f80509b183e568c
TainaBarreto27 <tainabarreto1205@gmail.com>
2024-04-26 16:19:20 -0300

    acerto ordem de print e escolher_frota

--- funcao_escfrota.py
+++ funcao_escfrota.py
@@ -1,5 +1,3 @@
-numero_frota = int(input("Qual o número da nação da sua frota?  ")) 
-
 def escolher_frota(numero_frota): 
     if numero_frota == 1: 
         mensagem = "Você escolheu a nação Brasil!"
@@ -9,25 +7,28 @@ def escolher_frota(numero_frota):
     
     elif numero_frota == 2: 
         mensagem = "Você escolheu a nação França!" 
+        espaco = "\n"
         mensagem_alocar = "Agora é a sua vez de alocar os seus navios!" 
         print (mensagem,espaco, mensagem_alocar)  
     
     elif numero_frota == 3: 
         mensagem = "Você escolheu a nação Austrália!" 
+        espaco = "\n"
         mensagem_alocar = "Agora é a sua vez de alocar os seus navios!" 
         print (mensagem,espaco, mensagem_alocar)
     
     elif numero_frota == 4: 
         mensagem = "Você escolheu a nação Rússia!" 
+        espaco = "\n"
         mensagem_alocar = "Agora é a sua vez de alocar os seus navios!" 
         print (mensagem, espaco, mensagem_alocar)
     
     elif numero_frota == 5: 
         mensagem = "Você escolheu a nação Japão!" 
+        espaco = "\n"
         mensagem_alocar = "Agora é a sua vez de alocar os seus navios!" 
         print (mensagem, espaco, mensagem_alocar) 
     
-print(escolher_frota(numero_frota)) 
 
         
 
---
+++ main.py
@@ -0,0 +1,28 @@
+import time 
+from base_geral import * 
+from funcao_escfrota import * 
+
+print("=================================") 
+print("Bem-vindo ao INSPER-Batalha Naval!!") 
+print("=================================") 
+
+time.sleep(0.5)
+
+print("Iniciando o jogo!") 
+
+#Trazer a função da Evilyn 
+
+time.sleep(0.5)
+
+#print("O computador está alocando os navios de guerra do país {0}". format()) 
+#print("\n")
+#print("Computador já está em posição de batalha") 
+print(PAISES)
+print("Agora é a sua vez de alocar seus navios de guerra")  
+
+numero_frota = int(input("Qual o número da nação da sua frota?  ")) 
+print(escolher_frota(numero_frota))
+
+
+
+
