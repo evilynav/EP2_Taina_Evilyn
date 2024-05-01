ca51c589921f287aedc4f201943aaf30908a23b8
evilynav <evilynav@al.insper.edu.br>
2024-05-01 19:01:31 -0300

    aloca_navios

--- terceira_funcao.py
+++ terceira_funcao.py
@@ -1,4 +1,12 @@
 import random
+from base_geral import *
+from funcoes import * 
+
+paism, embarcacoesm = random.choice(list(PAISES.items())) #pais sorteado e lista das embarcações disponíveis para tal país
+
+blocos_lista = lista_blocos(embarcacoesm,CONFIGURACAO) #funcao que devolve a lista de blocos disponível por barco disponível
+
+mapa = cria_mapa(10) #precisa mudar a funcao que a variável mapa recebe
 
 def posicao_suporta(mapa, blocos, linha, coluna, orientacao): 
 
@@ -18,24 +26,44 @@ def posicao_suporta(mapa, blocos, linha, coluna, orientacao):
                 return False 
     return True
 
-def aloca_navios (mapa, blocos):
+'''def aloca_navios(mapa, blocos):
     n = len(mapa)
-    #escolha das variáveis pela máquina:
-    
     for size_navio in blocos:
-        linha = random.randint(0, n-1)
-        coluna = random.randint(0, n-1)
+        linha = random.randint(0, n - 1)
+        coluna = random.randint(0, n - 1)
         orientacao = random.choice(['h', 'v'])
 
         while not posicao_suporta(mapa, size_navio, linha, coluna, orientacao):
-            linha = random.randint(0, n-1)
-            coluna = random.randint(0, n-1)
+            linha = random.randint(0, n - 1)
+            coluna = random.randint(0, n - 1)
             orientacao = random.choice(['h', 'v'])
 
         if orientacao == 'h':
-            for j in range(coluna, size_navio + coluna):
+            for j in range(coluna, coluna + size_navio):
                 mapa[linha][j] = 'N'
         else:
-            for i in range(linha, size_navio+ linha):
+            for i in range(linha, linha + size_navio):
                 mapa[i][coluna] = 'N'
+    return mapa'''
+
+def aloca_navios(mapa, blocos):
+    n = len(mapa)
+    for blocos_navio in blocos:
+        while True:
+            linha = random.randint(0, n - 1)
+            coluna = random.randint(0, n - 1)
+            orientacao = random.choice(['h', 'v'])
+
+            if posicao_suporta(mapa, blocos_navio, linha, coluna, orientacao):
+
+                if orientacao == 'h':
+                    for i in range(blocos_navio):
+                        mapa[linha][coluna + i] = 'N'
+                else:
+                    for i in range(blocos_navio):
+                        mapa[linha + i][coluna] = 'N'
+                break
+    
     return mapa
+
+print(aloca_navios(mapa,blocos_lista))
